09216e54255d30d17099adcd29e9c7fd25caaceb
Pavel Volkov <potschta.00@mail.ru>
2022-10-18 19:17:40 +0200

    main.c: changed output pin from C13 to B12

--- main.c
+++ main.c
@@ -11,15 +11,15 @@ int __attribute((noreturn)) main(void) {
 	// Enable clock for AFIO
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
 	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+	GPIOB->CRH &= ~GPIO_CRH_CNF12; //clear cnf bits
+	GPIOB->CRH |= GPIO_CRH_MODE12_0; //Max speed = 10Mhz
 
     while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
+	    GPIOB->ODR |= (1U<<12U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
 		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
+	    GPIOB->ODR &= ~(1U<<12U);
 	    delay(1000000);
     }
 }
