ef74501a0244faee56840e2732882db5728b0af0
Pavel Volkov <potschta.00@mail.ru>
2022-10-25 19:11:12 +0200

    push button to spot flashing

--- main.c
+++ main.c
@@ -8,15 +8,20 @@ int __attribute((noreturn)) main(void) {
 	// Enable clock for AFIO
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
 	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 	// Enable PC13 push-pull mode
-	GPIOB->CRH &= ~GPIO_CRH_CNF12; //clear cnf bits
-	GPIOB->CRH |= GPIO_CRH_MODE12_0; //Max speed = 10Mhz
-
+	GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); //clear cnf bits
+	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz 
+									 //General purpose Push-pull [00]
+	GPIOC->CRH = GPIOC->CRH & ~(GPIO_CRH_CNF14 | GPIO_CRH_MODE14) | GPIO_CRH_MODE14_1;
+	GPIOC->ODR |= GPIO_ODR_ODR14; //enable PC14 Pull-up
     while (1) {
-	    GPIOB->ODR |= (1U<<12U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOB->ODR &= ~(1U<<12U);
-	    delay(1000000);
+		while (!(GPIOC->IDR & GPIO_IDR_IDR14)) {
+			GPIOC->ODR &= ~(1U<<13U); //GPIOC->ODR &= ~GPIO_ODR_ODR13;
+									//U -- unsigned suffix (to avoid syntax warnings in IDE)
+			delay(10000000);
+			GPIOC->ODR |= (1U<<13U);  //GPIOC->ODR |= GPIO_ODR_ODR13;
+			delay(10000000);		  //for(uint32_t i = 0; i < 300000; i++) { __NOP(); }
+		}	  
     }
 }
