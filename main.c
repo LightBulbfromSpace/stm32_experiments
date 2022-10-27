83a7e2b9468dec1c0cb16b0712461d7246feb3df
Pavel Volkov <potschta.00@mail.ru>
2022-10-27 19:37:07 +0200

    main.c: made better response to button

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <utils.h>
 #include <stm32f10x.h>
 
+#define min(a,b) ((a) < (b) ? (a) : (b))
+
 
 
 int __attribute((noreturn)) main(void) {
@@ -11,24 +13,37 @@ int __attribute((noreturn)) main(void) {
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 	// Enable PC13 push-pull mode
 	GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz 
+	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
 									 //General purpose Push-pull [00]
 	GPIOC->CRH = GPIOC->CRH & ~(GPIO_CRH_CNF14 | GPIO_CRH_MODE14) | GPIO_CRH_MODE14_1;
 	GPIOC->ODR |= GPIO_ODR_ODR14; //enable PC14 Pull-up
-	uint8_t button_pushed = 0xFF;
+	uint32_t ledPeriod = 1000000;
+	uint32_t btnPeriod = 10000;
+	uint32_t ledPhase = ledPeriod;
+	uint32_t btnPhase = btnPeriod;
+	_Bool ledEnabled = 1;
+	_Bool buttonPrevState = GPIOC->IDR & (1 << 13U);
     while (1) {
-		if (!(GPIOC->IDR & GPIO_IDR_IDR14))
+		uint32_t tau = min(btnPhase, ledPhase);
+		delay_us(tau);
+		ledPhase -= tau;
+		btnPhase -= tau;
+		if (btnPhase == 0)
 		{
-			button_pushed = ~button_pushed;
-			delay(300000);			  //Debouncing (for this code isn't necessary)
-			while (!(GPIOC->IDR & GPIO_IDR_IDR14));
+			btnPhase = btnPeriod;
+			_Bool btnNewState = !(GPIOC->IDR & GPIO_IDR_IDR14);
+			if (!btnNewState && buttonPrevState) {
+				ledEnabled = !ledEnabled;
+			}
+			buttonPrevState = btnNewState;
 		}
-		if (button_pushed) {
-			GPIOC->ODR &= ~(1U<<13U); //GPIOC->ODR &= ~GPIO_ODR_ODR13;
-									  //U -- unsigned suffix (to avoid syntax warnings in IDE)
-			delay_us(1000000);
-			GPIOC->ODR |= (1U<<13U);  //GPIOC->ODR |= GPIO_ODR_ODR13;
-			delay_us(1000000);		 	  //for(uint32_t i = 0; i < 300000; i++) { __NOP(); }
+		if (ledPhase == 0) {
+			ledPhase = ledPeriod;
+			
+			if (ledEnabled) {
+				GPIOC->BSRR = ((GPIOC->ODR & GPIO_ODR_ODR13) << 16) | ( ~GPIOC->ODR & GPIO_ODR_ODR13);
+			}
+			
 		}
     }
 }
